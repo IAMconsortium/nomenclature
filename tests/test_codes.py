083502d2029798131cdcc715b404934c16f1fd24
Daniel Huppmann <dh@dergelbesalon.at>
2021-06-17 06:53:09 +0200

    Add a test that duplicate codes across files raises

--- tests/test_codes.py
+++ tests/test_codes.py
@@ -1,3 +1,4 @@
+import pytest
 from nomenclature import CodeList
 
 from conftest import TEST_DATA_DIR
@@ -9,3 +10,9 @@ def test_simple_codelist():
 
     assert "Some Variable" in code
     assert code["Some Variable"]["unit"] is None  # this is a dimensionless variable
+
+
+def test_duplicate_code_raises():
+    """Check that code conflicts across different files raises"""
+    with pytest.raises(ValueError, match="Duplicate foo key: Some Variable"):
+        CodeList("foo").parse_files(TEST_DATA_DIR / "duplicate_code_raises")
