9cb1683f3c95e4264b54e469b4293337f8deb978
Daniel Huppmann <dh@dergelbesalon.at>
2021-06-23 13:01:14 +0200

    Import testing module when initializing the package

--- nomenclature/__init__.py
+++ nomenclature/__init__.py
@@ -1,8 +1,9 @@
 import logging
 
 from nomenclature.utils import *
-from nomenclature.codes import *
-from nomenclature.core import *
+from nomenclature.codes import CodeList
+from nomenclature.core import Nomenclature
+from nomenclature.testing import assert_valid_yaml
 
 
 # set up logging
