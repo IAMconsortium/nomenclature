425a09595e6183db09cbc41db2532b4f33c09185
Daniel Huppmann <dh@dergelbesalon.at>
2021-09-07 09:46:16 +0200

    Refactor class name per suggestion by @khaeru

--- nomenclature/__init__.py
+++ nomenclature/__init__.py
@@ -2,7 +2,7 @@ import logging
 
 from nomenclature.utils import *
 from nomenclature.codes import CodeList
-from nomenclature.core import Nomenclature, create_yaml_from_xlsx
+from nomenclature.core import DataStructureDefinition, create_yaml_from_xlsx
 from nomenclature.testing import assert_valid_yaml
 
 
