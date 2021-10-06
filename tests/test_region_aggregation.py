564ce4fb1bf26a49aadb75833bcfa5103bc78f43
phackstock <p.hackstock@gmx.at>
2021-10-06 10:34:34 +0200

    Update tests to reflect changes in mapping files

--- tests/test_region_aggregation.py
+++ tests/test_region_aggregation.py
@@ -10,22 +10,27 @@ from conftest import TEST_DATA_DIR
 test_folder = TEST_DATA_DIR / "region_aggregation"
 
 
-def test_properly_formatted_mapping():
-    # test that the properly formatted file is read and represented correctly
-    file = test_folder / "message_mapping_verbose.yml"
-    with open(file, "r") as f:
-        mapping = yaml.load(f)
-    ram = RegionAggregationMapping(**mapping)
+@pytest.mark.parametrize(
+    "mapping_file",
+    ["working_message_mapping_cr_dict.yml", "working_message_mapping_cr_list.yml"],
+)
+def test_mapping(mapping_file):
+    # Test that the file is read and represented correctly
+    ram = convert_region_mapping(test_folder / mapping_file)
     reference = {
         "model": "MESSAGEix-Materials 1.1",
         "native_regions": [
-            {"name": "AFR", "rename": "Africa"},
+            {"name": "EEU", "rename": "Central and Eastern Europe"},
+            {"name": "FSU", "rename": "Former Soviet Union"},
             {"name": "WEU", "rename": None},
         ],
         "common_regions": [
             {
-                "name": "Africa",
-                "constituent_regions": [{"name": "AFR", "rename": None}],
+                "name": "Eastern Europe, Caucasus and Central Asia",
+                "constituent_regions": [
+                    {"name": "EEU", "rename": None},
+                    {"name": "FSU", "rename": None},
+                ],
             },
             {
                 "name": "Europe",
@@ -36,15 +41,14 @@ def test_properly_formatted_mapping():
     assert ram.dict() == reference
 
 
-@pytest.mark.parametrize(
-    "mapping_file",
-    ["working_message_mapping_cr_dict.yml", "working_message_mapping_cr_list.yml"],
-)
-def test_current_format(mapping_file):
-    # test that the file formatted according to current spec
-    properly_formatted_file = test_folder / "message_mapping_verbose.yml"
-    with open(properly_formatted_file, "r") as f:
-        mapping = yaml.load(f)
-    ram_verbose = RegionAggregationMapping(**mapping)
-    ram_current = convert_region_mapping(test_folder / mapping_file)
-    assert ram_current == ram_verbose
+def test_model_only():
+    # test that a region mapping runs also with only a model
+    reference = {
+        "model": "MESSAGEix-Materials 1.1",
+        "native_regions": None,
+        "common_regions": None,
+    }
+    model_only_mapping = convert_region_mapping(
+        test_folder / "working_message_mapping_model_only.yml"
+    )
+    assert reference == model_only_mapping.dict()
