c02974a7fa93f071da5c302c0ce390ddd2125a1e
phackstock <p.hackstock@gmx.at>
2021-10-07 18:51:49 +0200

    Add tests for validation logic

--- tests/test_region_aggregation.py
+++ tests/test_region_aggregation.py
@@ -1,12 +1,15 @@
+from typing import Type, ValuesView
 import pytest
 import yaml
 from nomenclature.region_mapping_models import RegionAggregationMapping
+from jsonschema.exceptions import ValidationError
 
 from conftest import TEST_DATA_DIR
 
 test_folder = TEST_DATA_DIR / "region_aggregation"
 
 
+@pytest.mark.skip(reason="Testing only illegal files")
 def test_mapping():
     mapping_file = "working_mapping.yml"
     # Test that the file is read and represented correctly
@@ -37,13 +40,41 @@ def test_mapping():
     assert ram.dict() == reference
 
 
-@pytest.mark.skip(reason="No illegal files yet")
-@pytest.mark.parametrize("illegal_mapping", [""])
-def test_illegal_mappings(illegal_mapping):
-    # This is to test a few mappings that are illegal and should be found as such
-    assert False
+@pytest.mark.parametrize(
+    "file, error_type, error_msg_pattern",
+    [
+        (
+            "illegal_mapping_1.yml",
+            ValidationError,
+            ".*'Eastern Europe.*not.*'array'.*",
+        ),
+        (
+            "illegal_mapping_2.yml",
+            ValidationError,
+            "Additional properties are not allowed.*",
+        ),
+        (
+            "illegal_mapping_3.yml",
+            ValueError,
+            ".*Overlapping.*Eastern Europe, Caucasus.*",
+        ),
+        ("illegal_mapping_4.yml", ValueError, ".*Two or more.*Europe.*"),
+        ("illegal_mapping_5.yml", ValueError, ".*Two or more.*Europe.*"),
+        (
+            "illegal_mapping_6.yml",
+            ValueError,
+            ".*common regions.*Eastern Europe, Caucasus.*",
+        ),
+    ],
+)
+def test_illegal_mappings(file, error_type, error_msg_pattern):
+    # This is to test a few different failure conditions
+
+    with pytest.raises(error_type, match=error_msg_pattern):
+        RegionAggregationMapping.create_from_region_mapping(test_folder / file)
 
 
+@pytest.mark.skip(reason="Testing only illegal files")
 def test_model_only_mapping():
     # test that a region mapping runs also with only a model
     reference = {
