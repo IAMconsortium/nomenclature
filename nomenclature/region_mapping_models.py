0d28bb846b558fee302aa722b4e6ecdbf6d568a2
Philip Hackstock <p.hackstock@gmx.at>
2021-10-13 11:55:50 +0200

    Parse region-mapping config file (#22)
    Co-authored-by: Daniel Huppmann <dh@dergelbesalon.at>

---
+++ nomenclature/region_mapping_models.py
@@ -0,0 +1,106 @@
+from pathlib import Path
+from typing import Dict, List, Optional, Union
+from collections import Counter
+
+import yaml
+from jsonschema import validate
+from pydantic import BaseModel, validator, root_validator
+
+here = Path(__file__).parent.absolute()
+
+
+class NativeRegion(BaseModel):
+    name: str
+    rename: Optional[str]
+
+    @property
+    def target_native_region(self):
+        return self.rename if self.rename is not None else self.name
+
+
+class CommonRegion(BaseModel):
+    name: str
+    constituent_regions: List[NativeRegion]
+
+
+class RegionAggregationMapping(BaseModel):
+    model: str
+    native_regions: Optional[List[NativeRegion]]
+    common_regions: Optional[List[CommonRegion]]
+
+    @validator("native_regions")
+    def validate_native_regions(cls, v):
+        target_names = [nr.target_native_region for nr in v]
+        duplicates = [
+            item for item, count in Counter(target_names).items() if count > 1
+        ]
+        if duplicates:
+            raise ValueError(
+                f"Two or more native regions share the same name: {duplicates}"
+            )
+        return v
+
+    @validator("common_regions")
+    def validate_common_regions(cls, v):
+        names = [cr.name for cr in v]
+        duplicates = [item for item, count in Counter(names).items() if count > 1]
+        if duplicates:
+            raise ValueError(
+                f"Duplicated aggregation mapping to common regions: {duplicates}"
+            )
+        return v
+
+    @root_validator()
+    def check_illegal_renaming(cls, values):
+        """Check if any renaming overlaps with common regions"""
+        # Skip if only either native-regions or common-regions are specified
+        if values.get("native_regions") is None or values.get("common_regions") is None:
+            return values
+        native_region_names = {
+            nr.target_native_region for nr in values["native_regions"]
+        }
+        common_region_names = {cr.name for cr in values["common_regions"]}
+        overlap = list(native_region_names & common_region_names)
+        if overlap:
+            raise ValueError(
+                "Conflict between (renamed) native regions and aggregation mapping"
+                f" to common regions: {overlap}"
+            )
+        return values
+
+    @classmethod
+    def create_from_region_mapping(cls, file: Union[Path, str]):
+        SCHEMA_FILE = here / "validation_schemas" / "region_mapping_schema.yaml"
+        with open(file, "r") as f:
+            mapping_input = yaml.safe_load(f)
+        with open(SCHEMA_FILE, "r") as f:
+            schema = yaml.safe_load(f)
+
+        # Validate the input data using jsonschema
+        validate(mapping_input, schema)
+
+        # Reformat the "native_regions"
+        if "native_regions" in mapping_input:
+            native_region_list: List[Dict] = []
+            for nr in mapping_input["native_regions"]:
+                if isinstance(nr, str):
+                    native_region_list.append({"name": nr})
+                elif isinstance(nr, dict):
+                    native_region_list.append(
+                        {"name": list(nr)[0], "rename": list(nr.values())[0]}
+                    )
+            mapping_input["native_regions"] = native_region_list
+
+        # Reformat the "common_regions"
+        if "common_regions" in mapping_input:
+            common_region_list: List[Dict[str, List[Dict[str, str]]]] = []
+            for cr in mapping_input["common_regions"]:
+                cr_name = list(cr)[0]
+                common_region_list.append(
+                    {
+                        "name": cr_name,
+                        "constituent_regions": [{"name": x} for x in cr[cr_name]],
+                    }
+                )
+            mapping_input["common_regions"] = common_region_list
+        return cls(**mapping_input)
---
+++ tests/test_region_aggregation.py
@@ -0,0 +1,92 @@
+import pytest
+from nomenclature.region_mapping_models import RegionAggregationMapping
+from jsonschema.exceptions import ValidationError
+
+from conftest import TEST_DATA_DIR
+
+test_folder = TEST_DATA_DIR / "region_aggregation"
+
+
+def test_mapping():
+    mapping_file = "working_mapping.yaml"
+    # Test that the file is read and represented correctly
+    obs = RegionAggregationMapping.create_from_region_mapping(
+        test_folder / mapping_file
+    )
+    exp = {
+        "model": "model_a",
+        "native_regions": [
+            {"name": "region_a", "rename": "alternative_name_a"},
+            {"name": "region_b", "rename": "alternative_name_b"},
+            {"name": "region_c", "rename": None},
+        ],
+        "common_regions": [
+            {
+                "name": "common_region_1",
+                "constituent_regions": [
+                    {"name": "region_a", "rename": None},
+                    {"name": "region_b", "rename": None},
+                ],
+            },
+            {
+                "name": "common_region_2",
+                "constituent_regions": [{"name": "region_c", "rename": None}],
+            },
+        ],
+    }
+    assert obs.dict() == exp
+
+
+@pytest.mark.parametrize(
+    "file, error_type, error_msg_pattern",
+    [
+        (
+            "illegal_mapping_invalid_format_dict.yaml",
+            ValidationError,
+            ".*common_region_1.*not.*'array'.*",
+        ),
+        (
+            "illegal_mapping_illegal_attribute.yaml",
+            ValidationError,
+            "Additional properties are not allowed.*",
+        ),
+        (
+            "illegal_mapping_conflict_regions.yaml",
+            ValueError,
+            r".*Conflict between \(renamed\).*common_region_1.*",
+        ),
+        (
+            "illegal_mapping_duplicate_native.yaml",
+            ValueError,
+            ".*Two or more.*alternative_name_a.*",
+        ),
+        (
+            "illegal_mapping_duplicate_native_rename.yaml",
+            ValueError,
+            ".*Two or more.*alternative_name_a.*",
+        ),
+        (
+            "illegal_mapping_duplicate_common.yaml",
+            ValueError,
+            ".*common regions.*common_region_1.*",
+        ),
+    ],
+)
+def test_illegal_mappings(file, error_type, error_msg_pattern):
+    # This is to test a few different failure conditions
+
+    with pytest.raises(error_type, match=error_msg_pattern):
+        RegionAggregationMapping.create_from_region_mapping(test_folder / file)
+
+
+def test_model_only_mapping():
+    # test that a region mapping runs also with only a model
+    exp = {
+        "model": "model_a",
+        "native_regions": None,
+        "common_regions": None,
+    }
+    obs = RegionAggregationMapping.create_from_region_mapping(
+        test_folder / "working_mapping_model_only.yaml"
+    )
+    assert exp == obs.dict()
