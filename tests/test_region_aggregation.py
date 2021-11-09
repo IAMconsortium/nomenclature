c7c844610f471a3d47b809b74c416c2131f5eb68
phackstock <p.hackstock@gmx.at>
2021-11-09 17:48:37 +0100

    Add first tests for RegionProcessor

--- tests/test_region_aggregation.py
+++ tests/test_region_aggregation.py
@@ -1,12 +1,15 @@
+import copy
 import jsonschema
 import pydantic
 import pytest
+import pandas as pd
+from nomenclature.core import DataStructureDefinition
 from nomenclature.region_mapping_models import (
     RegionAggregationMapping,
     RegionProcessor,
     ModelMappingCollisionError,
 )
-
+from pyam import IAMC_IDX, assert_iamframe_equal, IamDataFrame
 from conftest import TEST_DATA_DIR
 
 TEST_FOLDER_REGION_MAPPING = TEST_DATA_DIR / "region_aggregation"
@@ -174,3 +177,81 @@ def test_region_processor_wrong_args(simple_nomenclature):
             TEST_DATA_DIR / "regionprocessor_working/mapping_1.yaml",
             simple_nomenclature,
         )
+
+
+def test_region_processing_rename():
+    # Test **only** the renaming aspect, i.e. 3 things:
+    # 1. All native regions **with** a renaming property should be renamed correctly
+    # 2. All native regions **without** a renaming property should be passed through
+    # 3. All regions which are explicitly named should be dropped
+    # Testing strategy:
+    # 1. Rename region_a -> region_A
+    # 2. Leave region_B untouched
+    # 3. Drop region_C
+
+    test_df = IamDataFrame(
+        pd.DataFrame(
+            [
+                ["model_a", "scen_a", "region_a", "Primary Energy", "EJ/yr", 1, 2],
+                ["model_a", "scen_a", "region_B", "Primary Energy", "EJ/yr", 3, 4],
+                ["model_a", "scen_a", "region_C", "Primary Energy", "EJ/yr", 5, 6],
+            ],
+            columns=IAMC_IDX + [2005, 2010],
+        )
+    )
+
+    exp = copy.deepcopy(test_df)
+    exp.filter(region=["region_a", "region_B"], inplace=True)
+    exp.rename(region={"region_a": "region_A"}, inplace=True)
+
+    dsd = DataStructureDefinition(TEST_DATA_DIR / "region_processing/dsd")
+    rp = RegionProcessor.from_directory(
+        TEST_DATA_DIR / "region_processing/rename_only", dsd
+    )
+    obs = rp.apply(test_df)
+
+    assert_iamframe_equal(obs, exp)
+
+
+def test_region_processing_no_mapping(simple_df):
+    # Test that a model without a mapping is passed untouched
+
+    exp = copy.deepcopy(simple_df)
+    dsd = DataStructureDefinition(TEST_DATA_DIR / "region_processing/dsd")
+    rp = RegionProcessor.from_directory(
+        TEST_DATA_DIR / "region_processing/no_mapping", dsd
+    )
+    obs = rp.apply(simple_df)
+    assert_iamframe_equal(obs, exp)
+
+
+def test_region_processing_aggregate():
+    # Test only the aggregation feature
+    test_df = IamDataFrame(
+        pd.DataFrame(
+            [
+                ["model_a", "scen_a", "region_A", "Primary Energy", "EJ/yr", 1, 2],
+                ["model_a", "scen_a", "region_B", "Primary Energy", "EJ/yr", 3, 4],
+                ["model_a", "scen_a", "region_C", "Primary Energy", "EJ/yr", 5, 6],
+                ["model_a", "scen_b", "region_A", "Primary Energy", "EJ/yr", 1, 2],
+                ["model_a", "scen_b", "region_B", "Primary Energy", "EJ/yr", 3, 4],
+            ],
+            columns=IAMC_IDX + [2005, 2010],
+        )
+    )
+    exp = IamDataFrame(
+        pd.DataFrame(
+            [
+                ["model_a", "scen_a", "World", "Primary Energy", "EJ/yr", 4, 6],
+                ["model_a", "scen_b", "World", "Primary Energy", "EJ/yr", 4, 6],
+            ],
+            columns=IAMC_IDX + [2005, 2010],
+        )
+    )
+
+    dsd = DataStructureDefinition(TEST_DATA_DIR / "region_processing/dsd")
+    rp = RegionProcessor.from_directory(
+        TEST_DATA_DIR / "region_processing/aggregate_only", dsd
+    )
+    obs = rp.apply(test_df)
+    assert_iamframe_equal(obs, exp)
