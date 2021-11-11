474d2304134be46c62a762b04b24a510100c49e2
phackstock <p.hackstock@gmx.at>
2021-11-11 15:16:07 +0100

    Final # noqa adjustment

--- nomenclature/__init__.py
+++ nomenclature/__init__.py
@@ -3,9 +3,9 @@ import logging
 from nomenclature.codes import CodeList  # noqa
 from nomenclature.core import DataStructureDefinition, create_yaml_from_xlsx  # noqa
 from nomenclature.testing import assert_valid_yaml  # noqa
-from nomenclature.region_mapping_models import (
-    RegionProcessor,  # noqa
-    RegionAggregationMapping,  # noqa
+from nomenclature.region_mapping_models import (  # noqa
+    RegionProcessor,
+    RegionAggregationMapping,
 )
 
 
