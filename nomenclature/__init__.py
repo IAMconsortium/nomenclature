de5ebff3e93e55e624a9ce0a36b533e90e5c1019
phackstock <p.hackstock@gmx.at>
2021-11-11 14:47:45 +0100

    Add RegionProcessor and RegionAggregationMapping

--- nomenclature/__init__.py
+++ nomenclature/__init__.py
@@ -3,6 +3,7 @@ import logging
 from nomenclature.codes import CodeList  # noqa
 from nomenclature.core import DataStructureDefinition, create_yaml_from_xlsx  # noqa
 from nomenclature.testing import assert_valid_yaml  # noqa
+from nomenclature.region_mapping_models import RegionProcessor, RegionAggregationMapping
 
 
 # set up logging
