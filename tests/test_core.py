225833d2e4c24c38e3c0f7d7fcaa608573f53db2
phackstock <p.hackstock@gmx.at>
2021-12-10 11:57:58 +0100

    Add tests for core.process

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,80 +1,203 @@
-import pytest
+import copy
+
 import pandas as pd
-from nomenclature import DataStructureDefinition, create_yaml_from_xlsx
+from nomenclature.core import process
+from nomenclature.definition import DataStructureDefinition
+from nomenclature.processor.region import RegionProcessor
+from pyam import IAMC_IDX, IamDataFrame, assert_iamframe_equal
 
 from conftest import TEST_DATA_DIR
 
 
-def test_definition_with_custom_dimension(simple_definition):
-    """Check initializing a DataStructureDefinition with a custom dimension"""
+def test_region_processing_rename():
+    # Test **only** the renaming aspect, i.e. 3 things:
+    # 1. All native regions **with** a renaming property should be renamed correctly
+    # 2. All native regions **without** a renaming property should be passed through
+    # 3. All regions which are explicitly named should be dropped
+    # Testing strategy:
+    # 1. Rename region_a -> region_A
+    # 2. Leave region_B untouched
+    # 3. Drop region_C
 
-    obs = DataStructureDefinition(
-        TEST_DATA_DIR / "custom_dimension_nc",
-        dimensions=["region", "variable", "scenario"],
-    )
-
-    # check that "standard" dimensions are identical to simple test definitions
-    assert obs.region == simple_definition.region
-    assert obs.variable == simple_definition.variable
-
-    # check that "custom" dimensions are as expected
-    file = "scenario/scenarios.yaml"
-    assert obs.scenario["scen_a"] == {"attribute": "value", "file": file}
-    assert obs.scenario["scen_b"] == {"file": file}
-
-
-def test_nonexisting_path_raises():
-    """Check that initializing a DataStructureDefinition with non-existing path fails"""
-    match = "Definitions directory not found: foo"
-    with pytest.raises(NotADirectoryError, match=match):
-        DataStructureDefinition("foo")
-
-
-def test_empty_codelist_raises():
-    """Check that initializing a DataStructureDefinition with empty CodeList raises"""
-    match = "Empty codelist: region, variable"
-    with pytest.raises(ValueError, match=match):
-        DataStructureDefinition(TEST_DATA_DIR / "simple_codelist")
-
-
-def test_to_excel(simple_definition, tmpdir):
-    """Check writing a DataStructureDefinition to file"""
-    file = tmpdir / "testing_export.xlsx"
-
-    simple_definition.to_excel(file)
-
-    obs = pd.read_excel(file)
-    exp = pd.read_excel(TEST_DATA_DIR / "validation_nc.xlsx")
-    pd.testing.assert_frame_equal(obs, exp)
-
-
-def test_create_yaml_from_xlsx(tmpdir):
-    """Check that creating a yaml codelist from xlsx yields the expected output file"""
-    file = tmpdir / "foo.yaml"
-
-    create_yaml_from_xlsx(
-        source=TEST_DATA_DIR / "validation_nc.xlsx",
-        target=file,
-        sheet_name="variable_definitions",
-        col="Variable",
-        attrs=["Definition", "Unit"],
-    )
-
-    with open(file, "r") as f:
-        obs = f.read()
-    with open(TEST_DATA_DIR / "validation_nc_flat.yaml", "r") as f:
-        exp = f.read()
-
-    assert obs == exp
-
-
-def test_create_yaml_from_xlsx_duplicate():
-    """Check that creating a yaml codelist from xlsx with duplicates raises"""
-    with pytest.raises(ValueError, match="Duplicate values in the codelist:"):
-        create_yaml_from_xlsx(
-            source=TEST_DATA_DIR / "validation_nc_duplicates.xlsx",
-            target="_",
-            sheet_name="duplicate_index_raises",
-            col="Variable",
-            attrs=["Unit", "Definition"],
+    test_df = IamDataFrame(
+        pd.DataFrame(
+            [
+                ["model_a", "scen_a", "region_a", "Primary Energy", "EJ/yr", 1, 2],
+                ["model_a", "scen_a", "region_B", "Primary Energy", "EJ/yr", 3, 4],
+                ["model_a", "scen_a", "region_C", "Primary Energy", "EJ/yr", 5, 6],
+            ],
+            columns=IAMC_IDX + [2005, 2010],
         )
+    )
+
+    exp = copy.deepcopy(test_df)
+    exp.filter(region=["region_a", "region_B"], inplace=True)
+    exp.rename(region={"region_a": "region_A"}, inplace=True)
+
+    obs = process(
+        test_df,
+        DataStructureDefinition(TEST_DATA_DIR / "region_processing/dsd"),
+        processor=RegionProcessor.from_directory(
+            TEST_DATA_DIR / "region_processing/rename_only"
+        ),
+    )
+
+    assert_iamframe_equal(obs, exp)
+
+
+def test_region_processing_no_mapping(simple_df):
+    # Test that a model without a mapping is passed untouched
+
+    exp = copy.deepcopy(simple_df)
+
+    obs = process(
+        simple_df,
+        DataStructureDefinition(TEST_DATA_DIR / "region_processing/dsd"),
+        processor=RegionProcessor.from_directory(
+            TEST_DATA_DIR / "region_processing/no_mapping"
+        ),
+    )
+    assert_iamframe_equal(obs, exp)
+
+
+def test_region_processing_aggregate():
+    # Test only the aggregation feature
+    test_df = IamDataFrame(
+        pd.DataFrame(
+            [
+                ["model_a", "scen_a", "region_A", "Primary Energy", "EJ/yr", 1, 2],
+                ["model_a", "scen_a", "region_B", "Primary Energy", "EJ/yr", 3, 4],
+                ["model_a", "scen_a", "region_C", "Primary Energy", "EJ/yr", 5, 6],
+                ["model_a", "scen_b", "region_A", "Primary Energy", "EJ/yr", 1, 2],
+                ["model_a", "scen_b", "region_B", "Primary Energy", "EJ/yr", 3, 4],
+            ],
+            columns=IAMC_IDX + [2005, 2010],
+        )
+    )
+    exp = IamDataFrame(
+        pd.DataFrame(
+            [
+                ["model_a", "scen_a", "World", "Primary Energy", "EJ/yr", 4, 6],
+                ["model_a", "scen_b", "World", "Primary Energy", "EJ/yr", 4, 6],
+            ],
+            columns=IAMC_IDX + [2005, 2010],
+        )
+    )
+
+    obs = process(
+        test_df,
+        DataStructureDefinition(TEST_DATA_DIR / "region_processing/dsd"),
+        processor=RegionProcessor.from_directory(
+            TEST_DATA_DIR / "region_processing/aggregate_only"
+        ),
+    )
+
+    assert_iamframe_equal(obs, exp)
+
+
+def test_region_processing_complete():
+    # Test all three aspects of region processing together:
+    # 1. Renaming
+    # 2. Passing models without a mapping
+    # 3. Aggregating correctly
+
+    test_df = IamDataFrame(
+        pd.DataFrame(
+            [
+                ["m_a", "s_a", "region_a", "Primary Energy", "EJ/yr", 1, 2],
+                ["m_a", "s_a", "region_B", "Primary Energy", "EJ/yr", 3, 4],
+                ["m_a", "s_a", "region_C", "Primary Energy", "EJ/yr", 5, 6],
+                ["m_a", "s_a", "region_a", "Primary Energy|Coal", "EJ/yr", 0.5, 1],
+                ["m_a", "s_a", "region_B", "Primary Energy|Coal", "EJ/yr", 1.5, 2],
+                ["m_b", "s_b", "region_A", "Primary Energy", "EJ/yr", 1, 2],
+            ],
+            columns=IAMC_IDX + [2005, 2010],
+        )
+    )
+    exp = IamDataFrame(
+        pd.DataFrame(
+            [
+                ["m_a", "s_a", "region_A", "Primary Energy", "EJ/yr", 1, 2],
+                ["m_a", "s_a", "region_B", "Primary Energy", "EJ/yr", 3, 4],
+                ["m_a", "s_a", "World", "Primary Energy", "EJ/yr", 4, 6],
+                ["m_a", "s_a", "region_A", "Primary Energy|Coal", "EJ/yr", 0.5, 1],
+                ["m_a", "s_a", "region_B", "Primary Energy|Coal", "EJ/yr", 1.5, 2],
+                ["m_a", "s_a", "World", "Primary Energy|Coal", "EJ/yr", 2, 3],
+                ["m_b", "s_b", "region_A", "Primary Energy", "EJ/yr", 1, 2],
+            ],
+            columns=IAMC_IDX + [2005, 2010],
+        )
+    )
+
+    obs = process(
+        test_df,
+        DataStructureDefinition(TEST_DATA_DIR / "region_processing/dsd"),
+        processor=RegionProcessor.from_directory(
+            TEST_DATA_DIR / "region_processing/complete_processing"
+        ),
+    )
+    assert_iamframe_equal(obs, exp)
+
+
+def test_region_processing_weighted_aggregation():
+    # test a weighed sum
+
+    test_df = IamDataFrame(
+        pd.DataFrame(
+            [
+                ["model_a", "scen_a", "region_A", "Primary Energy", "EJ/yr", 1, 2],
+                ["model_a", "scen_a", "region_B", "Primary Energy", "EJ/yr", 3, 4],
+                ["model_a", "scen_a", "region_A", "Emissions|CO2", "Mt CO2", 4, 6],
+                ["model_a", "scen_a", "region_B", "Emissions|CO2", "Mt CO2", 1, 2],
+                ["model_a", "scen_a", "region_A", "Price|Carbon", "USD/t CO2", 3, 8],
+                ["model_a", "scen_a", "region_B", "Price|Carbon", "USD/t CO2", 2, 4],
+            ],
+            columns=IAMC_IDX + [2005, 2010],
+        )
+    )
+
+    exp = IamDataFrame(
+        pd.DataFrame(
+            [
+                ["model_a", "scen_a", "World", "Primary Energy", "EJ/yr", 4, 6],
+                ["model_a", "scen_a", "World", "Emissions|CO2", "Mt CO2", 5, 8],
+                ["model_a", "scen_a", "World", "Price|Carbon", "USD/t CO2", 2.8, 7.0],
+            ],
+            columns=IAMC_IDX + [2005, 2010],
+        )
+    )
+
+    obs = process(
+        test_df,
+        DataStructureDefinition(
+            TEST_DATA_DIR / "region_processing/weighted_aggregation/dsd"
+        ),
+        processor=RegionProcessor.from_directory(
+            TEST_DATA_DIR / "region_processing/weighted_aggregation/aggregate"
+        ),
+    )
+    assert_iamframe_equal(obs, exp)
+
+
+def test_region_processing_skip_aggregation():
+    test_df = IamDataFrame(
+        pd.DataFrame(
+            [
+                ["m_a", "s_a", "region_A", "Primary Energy", "EJ/yr", 1, 2],
+                ["m_a", "s_a", "region_B", "Primary Energy", "EJ/yr", 3, 4],
+            ],
+            columns=IAMC_IDX + [2005, 2010],
+        )
+    )
+    exp = test_df
+
+    obs = process(
+        test_df,
+        DataStructureDefinition(
+            TEST_DATA_DIR / "region_processing/skip_aggregation/dsd"
+        ),
+        processor=RegionProcessor.from_directory(
+            TEST_DATA_DIR / "region_processing/skip_aggregation/mappings"
+        ),
+    )
+    assert_iamframe_equal(obs, exp)
