0fab534404fbae9d23701ae996cd883cc999cbf3
phackstock <p.hackstock@gmx.at>
2021-12-13 18:04:58 +0100

    Add docstring for process

--- nomenclature/core.py
+++ nomenclature/core.py
@@ -1,5 +1,7 @@
 import copy
-from pyam import IamDataFrame
+from typing import List, Optional
+
+import pyam
 from pydantic import validate_arguments
 
 from nomenclature.definition import DataStructureDefinition
@@ -8,11 +10,49 @@ from nomenclature.processor.region import RegionProcessor
 
 @validate_arguments(config=dict(arbitrary_types_allowed=True))
 def process(
-    df: IamDataFrame,
+    df: pyam.IamDataFrame,
     dsd: DataStructureDefinition,
-    dimensions: list = None,
-    processor: RegionProcessor = None,
-):
+    dimensions: Optional[List[str]] = None,
+    processor: Optional[RegionProcessor] = None,
+) -> pyam.IamDataFrame:
+    """Function for validation and region aggregation in one step
+
+    Since there are some subtleties in the order of applying region processing and
+    validation, this function is the recommended way of using the nomenclature package
+    as it takes care of subtleties.
+
+    Parameters
+    ----------
+    df : pyam.IamDataFrame
+        Input data to be validated and aggregated.
+    dsd : DataStructureDefinition
+        Data templates that are used for validation.
+    dimensions : Optional[List[str]], optional
+        List of dimensions that will be used in the validation, if None all
+        dsd.dimensions will be used, by default None
+    processor : Optional[RegionProcessor], optional
+        Region processor that will perform region renaming and aggregation if provided,
+        by default None
+
+    Returns
+    -------
+    pyam.IamDataFrame
+        Processed data frame
+
+    Notes
+    -----
+    The above mentioned "subtleties" in the order of operations is related to the fact
+    that as part of the region processing, three things can occur:
+
+    1. Model native regions not mentioned in the model mapping will be dropped
+    2. Model native regions can be renamed
+    3. Common regions based on aggregated model native regions weill be newly created.
+
+    As each of these three can affects the outcome of the region validation, this
+    validation step needs to be performed *after* aggregation.
+    Since none of the other dimensions (e.g. variables, scenarios, etc...) are affected
+    by aggregation they can be validated before.
+    """
     # The deep copy is needed so we don't alter dsd in dimensions.remove("region")
     dimensions = copy.deepcopy(dimensions or dsd.dimensions)
     if processor is None:
