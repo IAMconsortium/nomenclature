6f84970b66bceb747d37a2db60eded60fe49edd3
Lucie <castella@iiasa.ac.at>
2022-08-10 11:52:07 +0200

    Implement comments
    Way to call the option: 
    --dimensions=variable,region,foo

--- nomenclature/cli.py
+++ nomenclature/cli.py
@@ -15,8 +15,10 @@ def cli_valid_yaml(path: Path):
 
 @cli.command("validate-project")
 @click.argument("path", type=click.Path(exists=True, path_type=Path))
-@click.argument("dimensions", nargs=-1)
+#@click.argument("dimensions", nargs=-1)
+@click.option('--dimensions', help='Optional list of dimensions', type=str)
 def cli_valid_project(path: Path, dimensions):
     """Assert that `path` is a valid project nomenclature"""
+    list_dimensions = list(dimensions.split(","))
     assert_valid_yaml(path)
-    assert_valid_structure(path, dimensions)
+    assert_valid_structure(path, list_dimensions)
