059d6ee70e05c289e1956b5e3fc4c130e2824717
Lucie <luciecastella@gmail.com>
2022-08-30 17:48:21 +0200

    Add schema parameter

--- nomenclature/codelist.py
+++ nomenclature/codelist.py
@@ -139,6 +139,8 @@ class CodeList(BaseModel):
             Directory with the codelist files
         file : str, optional
             Pattern to downselect codelist files by name
+        schema: str, optional
+            Schema to be used in the validation
 
         Returns
         -------
@@ -215,6 +217,8 @@ class CodeList(BaseModel):
             Directory with the codelist files
         file : str, optional
             Pattern to downselect codelist files by name
+        schema: str, optional
+            Schema to be used in the validation
 
         Returns
         -------
