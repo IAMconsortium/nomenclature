47e7e8b36c92d6b61b0c9331bc3db12d48b51fbf
Philip Hackstock <p.hackstock@gmx.at>
2022-11-11 13:38:01 +0100

    Add correct test for setting VaribleCode attributes with alias

--- tests/test_code.py
+++ tests/test_code.py
@@ -6,3 +6,18 @@ from nomenclature.code import VariableCode
 def test_variable_without_unit_raises():
     with pytest.raises(ValueError, match="unit\n.*required"):
         VariableCode(name="No unit")
+
+
+def test_variable_alias_setting():
+    assert (
+        VariableCode.from_dict(
+            {"Var1": {"unit": None, "skip_region_aggregation": True}}
+        ).skip_region_aggregation
+        is True
+    )
+    assert (
+        VariableCode.from_dict(
+            {"Var2": {"unit": None, "skip-region-aggregation": True}}
+        ).skip_region_aggregation
+        is True
+    )
