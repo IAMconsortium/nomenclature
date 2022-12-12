b8ecbf9cca10584ee11b504af8150ce6f5bf8c0a
Philip Hackstock <p.hackstock@gmx.at>
2022-12-12 17:37:48 +0100

    Add __init__ for processor sub module

---
+++ nomenclature/processor/__init__.py
@@ -0,0 +1,6 @@
+from nomenclature.processor.processor import Processor  # noqa
+from nomenclature.processor.region import (  # noqa
+    RegionAggregationMapping,
+    RegionProcessor,
+)
+from nomenclature.processor.requiredData import RequiredDataProcessor  # noqa
