875ea9518ddc1419a1e07fb646af9c5d5720e186
Philip Hackstock <20710924+phackstock@users.noreply.github.com>
2023-05-09 11:23:28 +0200

    Add Processor baseclass

--- nomenclature/processor/__init__.py
+++ nomenclature/processor/__init__.py
@@ -1,3 +1,15 @@
+import abc
+
+from pyam import IamDataFrame
+from pydantic import BaseModel
+
+
+class Processor(BaseModel, abc.ABC):
+    @abc.abstractmethod
+    def apply(self, df: IamDataFrame) -> IamDataFrame:
+        return
+
+
 from nomenclature.processor.region import (  # noqa
     RegionAggregationMapping,
     RegionProcessor,
