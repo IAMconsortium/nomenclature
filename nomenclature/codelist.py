7faa56fc9c1c6cde0bb6f63f07ee9e7932f8d17f
Philip Hackstock <20710924+phackstock@users.noreply.github.com>
2023-05-10 10:03:37 +0200

    Make CodeList.filter compatible with inherited classes

--- nomenclature/codelist.py
+++ nomenclature/codelist.py
@@ -406,11 +406,11 @@ class CodeList(BaseModel):
         # Returns True if code satisfies all filter parameters
         def _match_attribute(code, kwargs):
             return all(
-                hasattr(code, attribute) and getattr(code, attribute, None) == value
+                hasattr(code, attribute) and getattr(code, attribute) == value
                 for attribute, value in kwargs.items()
             )
 
-        filtered_codelist = CodeList(
+        filtered_codelist = self.__class__(
             name=self.name,
             mapping={
                 code.name: code
@@ -420,7 +420,7 @@ class CodeList(BaseModel):
         )
 
         if not filtered_codelist.mapping:
-            logging.warning("Formatted data is empty!")
+            logging.warning(f"Filtered {self.__class__.__name__} is empty!")
         return filtered_codelist
 
 
