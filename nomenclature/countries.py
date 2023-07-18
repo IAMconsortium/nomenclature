75f1740d4e00285b19be70342538fe76620df348
Daniel Huppmann <dh@dergelbesalon.at>
2023-07-18 12:32:18 +0200

    Add handling of non-standard alpha-2 codes used by European Commission

--- nomenclature/countries.py
+++ nomenclature/countries.py
@@ -1,5 +1,9 @@
 import os
+import logging
+
 import pycountry
+
+logger = logging.getLogger(__name__)
 
 # `nomenclature` uses pycountry to (optionally) add all countries and ISO3 codes
 # For readability and in line with conventions of the IAMC community,
@@ -24,16 +28,21 @@ PYCOUNTRY_NAME_OVERRIDE = {
 PYCOUNTRY_NAME_ADD = [
     dict(
         name="Kosovo",
-        note="Kosovo recognized under UNSC resolution 1244, alpha_3 code not assigned"
+        note="Kosovo recognized under UNSC resolution 1244, alpha_3 code not assigned",
     ),
 ]
+
+# the European Commission uses alternative ISO2 codes
+ALTERNATIVE_ALPHA2_CODES = {
+    "EL": "GR",
+    "UK": "GB",
+}
 
 
 class Countries(pycountry.ExistingCountries):
     """List of countries based on ISO 3166 database with simplications"""
 
     def __init__(self):
-
         super().__init__(os.path.join(pycountry.DATABASE_DIR, "iso3166-1.json"))
 
         # modify country names
@@ -59,6 +68,30 @@ class Countries(pycountry.ExistingCountries):
                 if value in index:
                     raise ValueError(f"Index conflict for country {key}: {value}")
                 index[value] = obj
+
+    def get(self, **kwargs):
+        """Get a specific country by attribute
+
+        Parameters
+        ----------
+        name : str
+            Country name
+        alpha_3 : str
+            Alpha-2 code (ISO3)
+        alpha_2 : str
+            Alpha-2 code (ISO2)
+        """
+        country = super().get(**kwargs)
+
+        # special handling for alpha-2 codes used by the European Commission
+        if country is None and "alpha_2" in kwargs:
+            country = super().get(alpha_2=ALTERNATIVE_ALPHA2_CODES[kwargs["alpha_2"]])
+            if country is not None:
+                logger.info(
+                    f"You are using non-standard alpha-2 codes for {country.name}."
+                )
+
+        return country
 
 
 # Initialize `countries` for direct access via API and in codelist module
