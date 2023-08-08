46286f350a4983b059a2279641f030964e5015bd
Philip Hackstock <20710924+phackstock@users.noreply.github.com>
2023-08-08 16:26:44 +0200

    Check if path is contained in cwd

--- nomenclature/processor/utils.py
+++ nomenclature/processor/utils.py
@@ -4,4 +4,8 @@ from pathlib import Path
 def get_relative_path(path: Path):
     # Get the relative version of `path` relative to `path_relative_to`
     # In case path does not contain `path_relative_to` it is returned unchanged
-    return path.relative_to(Path.cwd()) if path.is_absolute() else path
+    return (
+        path.relative_to(Path.cwd())
+        if path.is_absolute() and Path.cwd() in path.parents
+        else path
+    )
