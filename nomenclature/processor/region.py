cbb1fe2bde6a731f8dcaa0af4ebea118e55f6820
Philip Hackstock <20710924+phackstock@users.noreply.github.com>
2023-11-17 14:12:14 +0100

    Add feature to read RegionAggregationMapping from excel

--- nomenclature/processor/region.py
+++ nomenclature/processor/region.py
@@ -232,8 +232,20 @@ class RegionAggregationMapping(BaseModel):
         This function is used to convert a model mapping yaml file into a dictionary
         which is used to initialize a RegionAggregationMapping.
         """
+
+        file = Path(file) if isinstance(file, str) else file
+        FILE_PARSERS = {
+            ".yaml": cls.from_yaml,
+            ".yml": cls.from_yaml,
+            ".xlsx": cls.from_excel,
+        }
+        if file.suffix in FILE_PARSERS:
+            return FILE_PARSERS[file.suffix](file)
+        raise ValueError(f"No parser implemented for {file.suffix}")
+
+    @classmethod
+    def from_yaml(cls, file: Path) -> "RegionAggregationMapping":
         try:
-            file = Path(file) if isinstance(file, str) else file
             with open(file, "r") as f:
                 mapping_input = yaml.safe_load(f)
 
@@ -272,6 +284,54 @@ class RegionAggregationMapping(BaseModel):
                 file=get_relative_path(file), error=str(error)
             ) from error
         return cls(**mapping_input)
+
+    @classmethod
+    def from_excel(cls, file) -> "RegionAggregationMapping":
+        try:
+            model = pd.read_excel(file, sheet_name="Model", usecols="B", nrows=1).iloc[
+                0, 0
+            ]
+
+            regions = pd.read_excel(file, sheet_name="Common-Region-Mapping", header=3)
+            regions = regions.drop(
+                columns=(c for c in regions.columns if c.startswith("Unnamed: "))
+            ).drop(index=0)
+            # replace nan with None
+            regions = regions.where(pd.notnull(regions), None)
+            native = "Native region (as reported by the model)"
+            rename = "Native region (after renaming)"
+            native_regions = [
+                NativeRegion(name=row[native], rename=row[rename])
+                for row in regions[[native, rename]].to_dict(orient="records")
+            ]
+            common_region_groups = [
+                r for r in regions.columns if r not in (native, rename)
+            ]
+            common_regions = [
+                CommonRegion(
+                    name=common_region,
+                    constituent_regions=constituent_regions.split(","),
+                )
+                for common_region_group in common_region_groups
+                for common_region, constituent_regions in regions[
+                    [native, common_region_group]
+                ]
+                .groupby(common_region_group)[native]
+                .apply(lambda x: ",".join(x))
+                .to_dict()
+                .items()
+            ]
+            # common_regions = [for common_region_group in common_region_groups for ]
+        except Exception as error:
+            raise RegionAggregationMappingParsingError(
+                file=get_relative_path(file), error=str(error)
+            ) from error
+        return cls(
+            model=model,
+            file=file,
+            native_regions=native_regions,
+            common_regions=common_regions,
+        )
 
     @property
     def all_regions(self) -> List[str]:
