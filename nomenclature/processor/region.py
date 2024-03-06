346b17fafd2f0a275c0b3cf89a50758cea549c33
Daniel Huppmann <dh@dergelbesalon.at>
2024-03-06 11:50:42 +0100

    Do not show full error message for missing weights (#330)

--- nomenclature/processor/region.py
+++ nomenclature/processor/region.py
@@ -735,10 +735,7 @@ def _aggregate_region(df, var, *regions, **kwargs):
     except ValueError as error:
         if str(error).startswith("Missing weights for the following data"):
             logger.info(
-                (
-                    f"Could not aggregate '{var}' for region '{regions[0]}' "
-                    f"({kwargs})\n{error}"
-                )
+                f"Could not aggregate '{var}' for region '{regions[0]}' ({kwargs})"
             )
         else:
             raise error
