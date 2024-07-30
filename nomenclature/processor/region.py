735e945623f2090eac421e1066d60b23bc30b3d5
Philip Hackstock <20710924+phackstock@users.noreply.github.com>
2024-07-30 18:18:15 +0200

    Move log error from validation.py to __init__.py

--- nomenclature/processor/region.py
+++ nomenclature/processor/region.py
@@ -22,12 +22,12 @@ from pydantic import (
 from pydantic.types import DirectoryPath, FilePath
 from pydantic_core import PydanticCustomError
 
+from nomenclature import log_error
 from nomenclature.codelist import RegionCodeList, VariableCodeList
 from nomenclature.definition import DataStructureDefinition
 from nomenclature.error import custom_pydantic_errors, ErrorCollector
 from nomenclature.processor import Processor
 from nomenclature.processor.utils import get_relative_path
-from nomenclature.validation import log_error
 
 logger = logging.getLogger(__name__)
 
