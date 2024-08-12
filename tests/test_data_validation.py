47342be701e6dae8697cc02c35f58b4409fd787c
Daniel Huppmann <dh@dergelbesalon.at>
2024-08-12 14:14:34 +0200

    Add tests for failing validation against the DataStructureDefinition

--- tests/test_data_validation.py
+++ tests/test_data_validation.py
@@ -1,3 +1,4 @@
+import pytest
 from conftest import TEST_DATA_DIR
 
 from nomenclature import DataStructureDefinition
@@ -23,3 +24,36 @@ def test_DataValidator_from_file():
     )
     obs = DataValidator.from_file(DATA_VALIDATION_TEST_DIR / "simple_validation.yaml")
     assert obs == exp
+
+    dsd = DataStructureDefinition(TEST_DATA_DIR / "validation" / "definition")
+    obs.validate_with_definition(dsd) is None
+
+
+@pytest.mark.parametrize(
+    "dimension, match",
+    [
+        ("region", r"regions.*not defined.*\n.*Asia"),
+        ("variable", r"variables.*not defined.*\n.*Final Energy\|Industry"),
+    ],
+)
+def test_DataValidator_validate_with_definition_raises(dimension, match):
+    # Testing two different failure cases
+    # 1. Undefined region
+    # 2. Undefined variable
+    # TODO Undefined unit
+
+    data_validator = DataValidator.from_file(
+        DATA_VALIDATION_TEST_DIR / f"validation_unknown_{dimension}.yaml"
+    )
+
+    # validating against a DataStructure with all dimensions raises
+    dsd = DataStructureDefinition(TEST_DATA_DIR / "validation" / "definition")
+    with pytest.raises(ValueError, match=match):
+        data_validator.validate_with_definition(dsd)
+
+    # validating against a DataStructure without the offending dimension passes
+    dsd = DataStructureDefinition(
+        TEST_DATA_DIR / "validation" / "definition",
+        dimensions=[dim for dim in ["region", "variable"] if dim != dimension]
+    )
+    assert data_validator.validate_with_definition(dsd) is None
