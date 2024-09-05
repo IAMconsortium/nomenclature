65f005c40787548f59a4e0d068c38fda424cad0c
Daniel Huppmann <dh@dergelbesalon.at>
2024-09-05 10:50:58 +0200

    Update model-registration template to include Kosovo codes (#377)

--- tests/test_countries.py
+++ tests/test_countries.py
@@ -8,7 +8,7 @@ from nomenclature import countries
     [("Bolivia", "Bolivia, Plurinational State of", "BOL")],
 )
 def test_countries_override(nc_name, iso_name, alpha_3):
-    """Check that countries renamed from ISO 3166 can be found in both directions"""
+    """Check that countries renamed from ISO 3166-1 can be found in both directions"""
 
     assert countries.get(name=nc_name).alpha_3 == "BOL"
     assert countries.get(name=iso_name).alpha_3 == "BOL"
@@ -18,12 +18,11 @@ def test_countries_override(nc_name, iso_name, alpha_3):
 
 
 def test_countries_add():
-    """Check that countries added to ISO 3166 can be found"""
+    """Check that countries added beyond ISO 3166-1 can be found"""
 
     assert countries.get(name="Kosovo").name == "Kosovo"
     assert countries.get(name="Kosovo").alpha_3 == "KOS"
-    with pytest.raises(AttributeError):
-        countries.get(name="Kosovo").alpha_2
+    assert countries.get(name="Kosovo").alpha_2 == "XK"
 
     assert "Kosovo" in countries.names
 
