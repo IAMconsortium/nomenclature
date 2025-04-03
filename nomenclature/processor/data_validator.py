11a305cc7ebe1f0998081f16d06a6e0bf369a8be
Daniel Huppmann <dh@dergelbesalon.at>
2025-04-03 17:36:22 +0200

    Simplify codebase

--- nomenclature/processor/data_validator.py
+++ nomenclature/processor/data_validator.py
@@ -234,25 +234,20 @@ class DataValidator(Processor):
         for item in content:
             # simple case where filter and criteria args are all given at top level
             if "validation" not in item:
-                filter_args = {
-                    k: item[k] for k in item if k in IamcDataFilter.model_fields
-                }
-                criteria_args = [
-                    {
-                        k: item[k]
-                        for k in item
-                        if k not in IamcDataFilter.model_fields
-                    }
-                ]
-                item = dict(**filter_args, validation=criteria_args)
+                item["validation"] = [dict()]
+
             # if some criteria args are given at top-level, add to "validation" list
-            else:
-                criteria = [k for k in item if k not in IamcDataFilter.model_fields and k != "validation"]
-                for k in criteria:
-                    value = item.pop(k)
-                    for criteria_item in item["validation"]:
-                         criteria_item[k] = value
+            criteria = [
+                k
+                for k in item
+                if k not in IamcDataFilter.model_fields and k != "validation"
+            ]
+            for k in criteria:
+                value = item.pop(k)
+                for criteria_item in item["validation"]:
+                    criteria_item[k] = value
             criteria_items.append(item)
+
         return cls(file=file, criteria_items=criteria_items, output_path=output_path)  # type: ignore
 
     def apply(self, df: IamDataFrame) -> IamDataFrame:
