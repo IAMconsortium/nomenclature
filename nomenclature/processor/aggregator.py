13d78d30ef68f72fb7c90b653a8b5da0c6dfebbb
David Almeida <58078834+dc-almeida@users.noreply.github.com>
2025-07-18 11:07:55 +0200

    Add `rename` to `Aggregator` (#496)
    * Add explicit classmethod decorator
    
    * Add `rename` option to `Aggregator`
    
    * Fix check to allow duplicate rename targets
    
    * Add clarifying comment; change error message

--- nomenclature/processor/aggregator.py
+++ nomenclature/processor/aggregator.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 import yaml
 from pyam import IamDataFrame
-from pydantic import BaseModel, field_validator, ValidationInfo
+from pydantic import BaseModel, field_validator, model_validator, ValidationInfo
 from pydantic.types import FilePath
 from pydantic_core import PydanticCustomError
 
@@ -25,12 +25,20 @@ class AggregationItem(BaseModel):
     components: list[str]
 
 
+class RenameItem(BaseModel):
+    """Item used for simple renaming"""
+
+    name: str
+    rename: str
+
+
 class Aggregator(Processor):
     """Aggregation or renaming of an IamDataFrame on a `dimension`"""
 
     file: FilePath
     dimension: str
     aggregate: list[AggregationItem]
+    rename: list[RenameItem]
 
     def apply(self, df: IamDataFrame) -> IamDataFrame:
         """Apply region processing
@@ -59,31 +67,60 @@ class Aggregator(Processor):
             for c in item.components:
                 rename_dict[c] = item.name
 
+        for item in self.rename:
+            rename_dict[item.name] = item.rename
+
         return rename_dict
 
     @field_validator("aggregate")
+    @classmethod
     def validate_target_names(cls, v, info: ValidationInfo):
         _validate_items([item.name for item in v], info, "Duplicate target")
         return v
 
-    @field_validator("aggregate")
-    def validate_components(cls, v, info: ValidationInfo):
-        # components have to be unique for creating rename-mapping (component -> target)
-        all_components = list()
-        for item in v:
-            all_components.extend(item.components)
-        _validate_items(all_components, info, "Duplicate component")
+    @field_validator("rename")
+    @classmethod
+    def validate_rename(cls, v, info: ValidationInfo):
+        _validate_items(
+            [item.name for item in v], info.data["file"], "Duplicate renaming"
+        )
         return v
 
     @field_validator("aggregate")
+    @classmethod
+    def validate_components(cls, v, info: ValidationInfo):
+        """Check components are unique for creating rename-mapping (component -> target)"""
+        all_components = list()
+        for item in v:
+            all_components.extend(item.components)
+        _validate_items(all_components, info.data["file"], "Duplicate component")
+        return v
+
+    @field_validator("aggregate")
+    @classmethod
     def validate_target_vs_components(cls, v, info: ValidationInfo):
-        # guard against having identical target and component
+        """Guard against having identical target and component"""
         _codes = list()
         for item in v:
             _codes.append(item.name)
             _codes.extend(item.components)
-        _validate_items(_codes, info, "Non-unique target and component")
+        _validate_items(_codes, info.data["file"], "Non-unique target and component")
         return v
+
+    @model_validator(mode="after")
+    def validate_aggregate_vs_rename(self):
+        """Check renaming items are not aggregation items"""
+        agg_components = [comp for item in self.aggregate for comp in item.components]
+        agg_targets = [item.name for item in self.aggregate]
+        rename_sources = [item.name for item in self.rename]
+        # deduplicate rename targets (which are allowed)
+        rename_targets = list({item.rename for item in self.rename})
+        _validate_items(
+            agg_components + agg_targets + rename_sources + rename_targets,
+            self.file,
+            "Aggregation items in renaming items",
+        )
+        return self
 
     @property
     def codes(self):
@@ -119,6 +156,9 @@ class Aggregator(Processor):
           - Target Value:
             - Source Value A
             - Source Value B
+        rename:
+          - Source Value C: Target Value
+          - Source Value D: Target Value
 
         """
         file = Path(file) if isinstance(file, str) else file
@@ -127,21 +167,27 @@ class Aggregator(Processor):
                 mapping_input = yaml.safe_load(f)
 
             aggregate_list: list[dict[str, list]] = []
-            for item in mapping_input["aggregate"]:
+            for item in mapping_input.get("aggregate", []):
                 # TODO explicit check that only one key-value pair exists per item
                 aggregate_list.append(
                     dict(name=list(item)[0], components=list(item.values())[0])
+                )
+            rename_list: list[dict[str, list]] = []
+            for item in mapping_input.get("rename", []):
+                rename_list.append(
+                    dict(name=list(item)[0], rename=list(item.values())[0])
                 )
         except Exception as error:
             raise ValueError(f"{error} in {get_relative_path(file)}") from error
         return cls(
             dimension=mapping_input["dimension"],
             aggregate=aggregate_list,  # type: ignore
+            rename=rename_list,
             file=get_relative_path(file),
         )
 
 
-def _validate_items(items, info, _type):
+def _validate_items(items: list, file: str, _type: str):
     duplicates = [item for item, count in Counter(items).items() if count > 1]
     if duplicates:
         raise PydanticCustomError(
@@ -149,6 +195,6 @@ def _validate_items(items, info, _type):
             {
                 "type": _type,
                 "duplicates": duplicates,
-                "file": info.data["file"],
+                "file": file,
             },
         )
