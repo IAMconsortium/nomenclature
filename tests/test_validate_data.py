17c50dadd502f13513b13dceaf3886abb46de0da
Daniel Huppmann <dh@dergelbesalon.at>
2025-07-24 07:50:01 +0200

    Make ruff

--- tests/test_validate_data.py
+++ tests/test_validate_data.py
@@ -143,7 +143,7 @@ def test_DataValidator_apply_no_matching_data(simple_df):
             "range: [1.0, 5.0]",
             "range: [2.0, 5.0]",
             "range: [1.1, 1.9]",
-        )
+        ),
     ],
 )
 def test_DataValidator_apply_fails(simple_df, file, item_1, item_2, item_3, caplog):
@@ -223,13 +223,12 @@ def test_DataValidator_validate_fail_with_warning(file, value, simple_df, caplog
 
     with pytest.raises(ValueError, match="Data validation failed"):
         data_validator.apply(simple_df)
-        
+
     # check if the log message contains the correct information
     assert failed_validation_message in caplog.text
 
 
 def test_DataValidator_pass_with_warning(simple_df, caplog):
-
     data_validator = DataValidator.from_file(
         DATA_VALIDATION_TEST_DIR / "validate_pass_warning.yaml"
     )
@@ -250,10 +249,10 @@ def test_DataValidator_pass_with_warning(simple_df, caplog):
     assert warning_message in caplog.text
 
     columns = [
-            "Vetting|Primary Energy [All]",
-            "Vetting|Primary Energy [2005]",
-            "Vetting|Coal [2010]",
-        ]
+        "Vetting|Primary Energy [All]",
+        "Vetting|Primary Energy [2005]",
+        "Vetting|Coal [2010]",
+    ]
     exp_meta = pd.DataFrame(
         [["low", "ok", "medium"], ["high", "medium", np.nan]],
         columns=columns,
@@ -294,7 +293,7 @@ def test_DataValidator_xlsx_output(tmp_path, simple_df):
             "year": [2010, 2010],
             "value": [6.0, 7.0],
             "warning_level": ["error", "error"],
-            "criteria": ["upper_bound: 5.0, lower_bound: 1.0"] * 2
+            "criteria": ["upper_bound: 5.0, lower_bound: 1.0"] * 2,
         }
     )
     pdt.assert_frame_equal(obs, exp, check_dtype=False)
