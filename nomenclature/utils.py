868839cdde65719e0a013620806b6705f14c897a
Daniel Huppmann <dh@dergelbesalon.at>
2025-11-12 11:33:01 +0100

    Allow non-Path names for `Processor` file attributes

--- nomenclature/utils.py
+++ nomenclature/utils.py
@@ -4,6 +4,8 @@ from pathlib import Path
 def get_relative_path(path: Path):
     # Get the relative version of `path` relative to `path_relative_to`
     # In case path does not contain `path_relative_to` it is returned unchanged
+    if isinstance(path, str):
+        return (path)
     return (
         path.relative_to(Path.cwd())
         if path.is_absolute() and Path.cwd() in path.parents
