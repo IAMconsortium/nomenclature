f30b7a52a3c26586b177cba00f20ececc06cb1e1
Philip Hackstock <20710924+phackstock@users.noreply.github.com>
2026-03-23 15:00:21 +0100

    Combine stderr and stdout for test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -326,11 +326,12 @@ def test_error_group_rendering():
         ],
         capture_output=True,
         text=True,
+        stderr=subprocess.STDOUT,
     )
     assert result.returncode == 1
-    assert "6 sub-exceptions" in result.stderr
-    assert "Asia" in result.stderr
-    assert "Final Energy|Industry" in result.stderr
+    assert "6 sub-exceptions" in result.stdout
+    assert "Asia" in result.stdout
+    assert "Final Energy|Industry" in result.stdout
 
 
 def test_cli_empty_definitions_dir():
